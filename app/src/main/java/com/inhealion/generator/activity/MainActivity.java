ae1a56ee8c8364ffb4e8db3c1c8492f066bfe825
steadxcz <vemcenko@gmail.com>
2021-02-07 16:56:37 +0200

    Added fragments, need to implement parceliable on client

--- app/src/main/java/com/inhealion/generator/activity/MainActivity.java
+++ app/src/main/java/com/inhealion/generator/activity/MainActivity.java
@@ -2,11 +2,20 @@ package com.inhealion.generator.activity;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.inhealion.generator.R;
+import com.inhealion.generator.folders;
+import com.inhealion.generator.log_in;
 import com.inhealion.networking.ApiCallback;
 import com.inhealion.networking.GeneratorApiClient;
 import com.inhealion.networking.api.model.User;
@@ -15,34 +24,25 @@ import org.jetbrains.annotations.NotNull;
 
 import timber.log.Timber;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+    private folders FoldersFragment;
+    private log_in LogInFragment;
 
     String zipFilename = Environment.getExternalStorageDirectory() + "/unzipped/files.zip";
     String unzipLocation = Environment.getExternalStorageDirectory() + "/unzipped";
-
+    GeneratorApiClient client = GeneratorApiClient.instance();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
-
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GeneratorApiClient client = GeneratorApiClient.instance();
-        Decompress d = new Decompress(zipFilename, unzipLocation);
-        d.unzip();
-
-        client.signIn("admin", "admin", new ApiCallback<User>() {
-            @Override
-            public void success(User user) {
-                Timber.d("Login Success%s", user);
-            }
-
-            @Override
-            public void failure(@NotNull Exception error) {
-                Timber.e(error, "Login Failed");
-            }
-        });
+        super.onCreate(savedInstanceState);
+        LogInFragment = new log_in();
+        FoldersFragment = new folders();
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("GeneratorApiClient", client); // There we should be able to pack client obj to bunble
+       getSupportFragmentManager().beginTransaction()
+                .replace(R.id.frame,LogInFragment)
+                .commit();
     }
 }
+
---
+++ app/src/main/java/com/inhealion/generator/log_in.java
@@ -0,0 +1,111 @@
+package com.inhealion.generator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.inhealion.networking.ApiCallback;
+import com.inhealion.networking.GeneratorApiClient;
+import com.inhealion.networking.api.model.Folder;
+import com.inhealion.networking.api.model.User;
+
+import org.jetbrains.annotations.NotNull;
+
+import timber.log.Timber;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link log_in#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class log_in extends Fragment {
+
+
+    private Button logInBtn;
+    private EditText unField, pwFiled;
+    private folders FoldersFragment;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public log_in() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment log_in.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static log_in newInstance(String param1, String param2) {
+        log_in fragment = new log_in();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_log_in, container, false);
+        logInBtn = v.findViewById( R.id.logInBtn);
+        unField = v.findViewById( R.id.unField);
+        pwFiled = v.findViewById( R.id.pwFiled);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String login = unField.getText().toString();
+                String password = pwFiled.getText().toString();
+                //there we should get client object from bundle
+                //GeneratorApiClient client = GeneratorApiClient.instance();
+                client.signIn(login, password, new ApiCallback<User>() {
+                    @Override
+                    public void success(User user) {
+                        FoldersFragment = new folders();
+                        getFragmentManager().beginTransaction()
+                                .replace(R.id.frame,FoldersFragment)
+                                .commit();
+                        Timber.d("Login Success%s", user);
+
+                    }
+
+                    @Override
+                    public void failure(@NotNull Exception error) {
+                        Timber.e(error, "Login Failed");
+                    }
+                });
+            }
+        });
+        return v;
+    }
+
+}
