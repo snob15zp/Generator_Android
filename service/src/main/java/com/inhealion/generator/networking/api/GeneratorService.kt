1c12ec8ad9846ca368f57a906bfce9c847aa6371
Alexander Baltser <aubalcer@gmail.com>
2021-10-15 22:02:39 +0300

    Change base url

--- service/src/main/java/com/inhealion/generator/networking/api/GeneratorService.kt
+++ service/src/main/java/com/inhealion/generator/networking/api/GeneratorService.kt
@@ -6,35 +6,35 @@ import retrofit2.http.*
 
 interface GeneratorService {
     @FormUrlEncoded
-    @POST("/api/users/login")
+    @POST("api/users/login")
     suspend fun login(@Field("login") login: String, @Field("password") password: String): User?
 
-    @GET("/api/profiles/{id}/folders")
+    @GET("api/profiles/{id}/folders")
     suspend fun fetchFolders(@Path("id") userProfileId: String): List<Folder>?
 
-    @GET("/api/folders/{id}/programs")
+    @GET("api/folders/{id}/programs")
     suspend fun fetchPrograms(@Path("id") folderId: String): List<Program>?
 
     @Streaming
-    @GET("/api/folders/{id}/download")
+    @GET("api/folders/{id}/download")
     suspend fun downloadFolder(@Path("id") folderId: String): ResponseBody?
 
     @Streaming
-    @GET("/api/firmware/{version}/download")
+    @GET("api/firmware/{version}/download")
     suspend fun downloadFirmware(@Path("version") version: String): ResponseBody?
 
-    @POST("/api/users/logout")
+    @POST("api/users/logout")
     suspend fun logout()
 
-    @GET("/api/users/{id}/profile")
+    @GET("api/users/{id}/profile")
     suspend fun fetchUserProfile(@Path("id") userId: String): UserProfile?
 
-    @PUT("/api/users/refresh")
+    @PUT("api/users/refresh")
     suspend fun refreshToken(): RefreshToken?
 
-    @GET("/api/firmware/latest")
+    @GET("api/firmware/latest")
     suspend fun getLatestFirmwareVersion(): FirmwareVersion
 
-    @GET("/api/folders/{id}")
+    @GET("api/folders/{id}")
     suspend fun fetchFolder(@Path("id") id: String): Folder
 }
